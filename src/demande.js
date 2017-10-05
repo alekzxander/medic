ee0b1d391830a454393fab0b84ecab32881106cc
Alexandre Callens <alekzxand@gmail.com>
2017-10-05 14:33:59 +0400

    g

--- src/demande.js
+++ src/demande.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-
+import bell from './bell.png'
 export default class Demande extends Component {
   render() {
     return (
@@ -8,9 +8,17 @@ export default class Demande extends Component {
             <h1>Demandes</h1>
         </div>
         <div className="mesDemandes">
+            <div className="row">
+                <div className="col-md-6">
+                    <img src={bell} alt="" />
+                </div>
+                <div className="col-md-6">
+                    <p>3 Notifications </p>
+                </div>
+            </div>
 
         </div>
-      </div>
+        </div>
     )
   }
 }
